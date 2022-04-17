b0c907d51447ec4ba1cca97e842f7ca68511616c
Mike-415 <mgonz160@mail.ccsf.edu>
2022-04-17 14:24:06 -0700

    Created the running_table

--- app/src/main/java/com/androiddevs/runningappyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/MainActivity.kt
@@ -2,11 +2,15 @@ package com.androiddevs.runningappyt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = "MainActivity"
+    private var KEY = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        KEY =  resources.getString(R.string.google_maps_key)
+        Log.i(TAG, KEY )
     }
 }
---
+++ app/src/main/java/com/androiddevs/runningappyt/db/Run.kt
@@ -0,0 +1,20 @@
+package com.androiddevs.runningappyt.db
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+//define the entity(aka a Table) for Room
+@Entity(tableName = "running_table")
+data class Run(
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L,   //What time the run started
+    var avgSpeedInMPH: Float = 0f,
+    var distanceInMPH: Int = 0,
+    var timeInMilis: Long = 0L, //how long was the run
+    var caloriesBurned: Int = 0,
+) {
+    //We put the primary key within the class body bc Room will automatically create it
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
